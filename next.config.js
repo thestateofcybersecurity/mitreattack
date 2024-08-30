20609ac9baefd08b3e4a36424d626bfc7f96481e
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-30 14:58:44 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_DB: process.env.MONGODB_DB || 'test',
+  },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
@@ -10,13 +14,7 @@ const nextConfig = {
       '/hackerSkillSheet': { page: '/hackerSkillSheet' },
       '/game': { page: '/game' },
     }
-  },
 }
 
-module.exports = {
-  env: {
-    MONGODB_URI: process.env.MONGODB_URI,
-    MONGODB_DB: process.env.MONGODB_DB,
-  },
-}
+module.exports = nextConfig
 
