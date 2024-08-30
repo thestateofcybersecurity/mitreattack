d09999a5b95f7c1c11c89cfb8e752ae566ed383f
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-30 09:38:14 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/hackerSkillSheet': { page: '/hackerSkillSheet' },
+      '/game': { page: '/game' },
+    }
+  },
 }
 
 module.exports = nextConfig
