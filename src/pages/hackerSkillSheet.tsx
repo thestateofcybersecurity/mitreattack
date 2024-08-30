df87f78e1fd30398f0ac1b3d524fb47a901f489c
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-08-29 21:17:31 -0600

    Create hackerSkillSheet.tsx

---
+++ src/pages/hackerSkillSheet.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HackerSkillSheet from '@/components/HackerSkillSheet';
+
+const HackerSkillSheetPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <HackerSkillSheet />
+    </div>
+  );
+};
+
+export default HackerSkillSheetPage;
